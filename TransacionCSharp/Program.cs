505ce50e6c5b08b33efbcb660f852868c4e6276a
AnaChamba <abm.chamba@gmail.com>
2020-08-18 15:21:26 -0500

    transacción mediante consola

---
+++ TransacionCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TransacionCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
